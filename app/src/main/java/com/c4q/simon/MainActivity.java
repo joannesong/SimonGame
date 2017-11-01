8d73d3b532d4e76c575b974b49ccaca7007c1ab2
Mohammad Naz <Mohammadnaz@ac.c4q.nyc>
2017-11-01 18:09:05 -0400

    alpha

--- app/src/main/java/com/c4q/simon/MainActivity.java
+++ app/src/main/java/com/c4q/simon/MainActivity.java
@@ -1,28 +1,37 @@
 package com.c4q.simon;
 
 import android.graphics.Color;
+import android.os.Handler;
 import android.support.annotation.ColorInt;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button[] button;
     Button[] simon;
-    private static int number;
+    AlphaAnimation buttonAnimation;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createButtonAnimation();
+
 
         button = new Button[4];
         simon = new Button[4];
+        ////List<Button> x = new ArrayList<>();
 
         button[0] = findViewById(R.id.red);
         button[0].setOnClickListener(this);
@@ -32,6 +41,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         button[2].setOnClickListener(this);
         button[3] = findViewById(R.id.yellow);
         button[3].setOnClickListener(this);
+        Collections.shuffle(Arrays.asList(button));
+        makeButtonFalse(button);
 
 
     }
@@ -42,63 +53,72 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         switch (view.getId()) {
             case R.id.red:
-
-                button[0].setBackgroundResource(R.color.lightred);
-                button[0].setBackgroundResource(R.color.red);
-                //Toast.makeText(getApplicationContext(), String.valueOf(number), Toast.LENGTH_SHORT).show();
-                simon[number++] = findViewById(view.getId());
-                //Toast.makeText(getApplicationContext(), String.valueOf(number), Toast.LENGTH_SHORT).show();
-
+                for (int i = 0; i < button.length; i++) {
+                    if (button[i].getId() == R.id.red) {
+                        button[i].startAnimation(buttonAnimation);
+                        simon[i] = findViewById(view.getId());
+                    }
+                }
 
 
                 break;
             case R.id.blue:
+                for (int i = 0; i < button.length; i++) {
+                    if (button[i].getId() == R.id.blue) {
+                        button[i].startAnimation(buttonAnimation);
+                        simon[i] = findViewById(view.getId());
+                    }
 
-                button[1].setBackgroundResource(R.color.lightblue);
-                button[1].setBackgroundResource(R.color.blue);
-                //Toast.makeText(getApplicationContext(), String.valueOf(number), Toast.LENGTH_SHORT).show();
-                simon[number++] = findViewById(view.getId());
-                //Toast.makeText(getApplicationContext(), String.valueOf(number), Toast.LENGTH_SHORT).show();
+                }
+
 
                 break;
             case R.id.green:
+                for (int i = 0; i < button.length; i++) {
+                    if (button[i].getId() == R.id.green) {
+                        button[i].startAnimation(buttonAnimation);
+                        simon[i] = findViewById(view.getId());
+                    }
 
-                button[2].setBackgroundResource(R.color.lightgreen);
-                button[2].setBackgroundResource(R.color.green);
-                //Toast.makeText(getApplicationContext(), String.valueOf(number), Toast.LENGTH_SHORT).show();
-                simon[number++] = findViewById(view.getId());
-                //Toast.makeText(getApplicationContext(), String.valueOf(number), Toast.LENGTH_SHORT).show();
+                }
+
 
                 break;
             case R.id.yellow:
-
-                button[3].setBackgroundResource(R.color.lightyellow);
-                button[3].setBackgroundResource(R.color.yellow);
-               // Toast.makeText(getApplicationContext(), String.valueOf(number), Toast.LENGTH_SHORT).show();
-                simon[number++] = findViewById(view.getId());
-                //Toast.makeText(getApplicationContext(), String.valueOf(number), Toast.LENGTH_SHORT).show();
+                for (int i = 0; i < button.length; i++) {
+                    if (button[i].getId() == R.id.yellow) {
+                        button[i].startAnimation(buttonAnimation);
+                        simon[i] = findViewById(view.getId());
+                    }
+                }
                 break;
             default:
                 break;
         }
     }
-    public void play(View view){
-        button[0].setBackgroundResource(R.color.lightred);
-        button[1].setBackgroundResource(R.color.lightblue);
-        button[2].setBackgroundResource(R.color.lightgreen);
-        button[3].setBackgroundResource(R.color.lightyellow);
-        reset();
 
+    public void play(View view) throws InterruptedException {
+        makeButtonTrue(button);
+        for (int i = 0; i < button.length; i++) {
+            final Handler handler = new Handler();
+            final int finalI = i;
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+
+                    button[finalI].startAnimation(buttonAnimation);
+                    button[finalI].setText(String.valueOf(finalI));
+
+
+                }
+            }, 1000 * i * 2);
+            Collections.shuffle(Arrays.asList(button));
+
+        }
 
 
     }
-    public void reset(){
-        button[0].setBackgroundResource(R.color.red);
-        button[1].setBackgroundResource(R.color.blue);
-        button[2].setBackgroundResource(R.color.green);
-        button[3].setBackgroundResource(R.color.yellow);
 
-    }
 
     public void checks(Button[] button, Button[] simon) {
         for (int i = 0; i < button.length; i++) {
@@ -110,8 +130,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     public void check(View view) {
         checks(button, simon);
+       // Arrays.fill(simon, null);
 
     }
+
+    private void createButtonAnimation() {
+        buttonAnimation = new AlphaAnimation(0.2f, 1f);
+        buttonAnimation.setDuration(1000);
+    }
+
+    public void makeButtonFalse(Button[] button){
+        for (int i = 0; i <button.length ; i++) {
+            button[i].setEnabled(false);
+
+        }
+    }
+
+    public void makeButtonTrue(Button[] button){
+        for (int i = 0; i <button.length ; i++) {
+            button[i].setEnabled(true);
+
+        }
+    }
+
 }
 
 
